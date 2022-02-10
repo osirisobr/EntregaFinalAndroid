9d364e32fad7cc354b7ce978cd67b6ff0357093f
osirisobr <92573970+osirisobr@users.noreply.github.com>
2022-02-10 03:48:11 +0100

    Boton cerrar sesion fallo

--- app/src/main/java/com/murallaromana/dam/segundo/ogandobritoosirisjuniorproyectopmdm/PeliculasActivity.kt
+++ app/src/main/java/com/murallaromana/dam/segundo/ogandobritoosirisjuniorproyectopmdm/PeliculasActivity.kt
@@ -5,8 +5,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -14,8 +17,14 @@ import com.murallaromana.dam.segundo.ogandobritoosirisjuniorproyectopmdm.Retrofi
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class PeliculasActivity : AppCompatActivity() {
+    companion object {
+        var itemCerrarSesion: MenuItem? = null
+    }
+
     private lateinit var rvPeliculas : RecyclerView
     private lateinit var faButton: FloatingActionButton
     private lateinit var adapter: ListaPeliculasAdapter
@@ -28,6 +37,8 @@ class PeliculasActivity : AppCompatActivity() {
         rvPeliculas=findViewById(R.id.rvPeliculas)
         rvPeliculas.layoutManager = LinearLayoutManager(this)
         faButton = findViewById(R.id.faButton)
+
+
 
 
 
@@ -81,6 +92,80 @@ class PeliculasActivity : AppCompatActivity() {
         } )
 //
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detalle_pelicula, menu)
+        if (menu != null) {
+            PeliculasActivity.itemCerrarSesion = menu.findItem(R.id.action_cerrarsesion)
+        }
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            R.id.action_borrar -> {
+                val context=this
+                val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                val dialog = builder.setTitle("Eliminar pelicula")
+                    .setMessage("Se cerrara la sesion. ¿Estas seguro?.")
+                    .setPositiveButton("Aceptar") { _, _ ->
+                        val sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE)
+                        val editor = sharedPreferences.edit()
+                        editor.apply() {
+                            putString("TOKEN", null)
+                        }.apply()
+
+                        val intent = Intent(context, PeliculasActivity::class.java)
+                        startActivity(intent)
+
+
+                        //Separacion
+
+
+
+
+                        Toast.makeText(this, "Sesion cerrada", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                    .setNegativeButton("Cancelar", null).create()
+                dialog.show()
+
+                return true
+
+            }
+
+
+
+
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     fun loadData() {
 
         val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
